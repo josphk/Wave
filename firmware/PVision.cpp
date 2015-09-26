a96fa6dfcc08832643188bd16e4780de68119fbf
Joseph Kim <jkim0120@hotmail.com>
2015-09-26 14:40:25 -0400

    Cleaned up Particle code

--- firmware/PVision.cpp
+++ firmware/PVision.cpp
@@ -1,40 +1,23 @@
-// Example of using the PVision library for interaction with the Pixart sensor on a WiiMote
-// This work was derived from Kako's excellent Japanese website
-// http://www.kako.com/neta/2007-001/2007-001.html
+// Library derived from Steve Hobley and Kako
 
-// Steve Hobley 2009 - www.stephenhobley.com
-/******************************************************************************
-* Includes
-******************************************************************************/
 #include "PVision.h"
 
-/******************************************************************************
-* Private methods
-******************************************************************************/
 void PVision::Write_2bytes(byte d1, byte d2) {
     Wire.beginTransmission(IRslaveAddress);
     Wire.write(d1); Wire.write(d2);
     Wire.endTransmission();
 }
 
-
-/******************************************************************************
-* Constructor
-******************************************************************************/
 PVision::PVision() {
-	Blob1.number = 1;
+	Point.number = 1;
 }
 
-/******************************************************************************
-* Public methods
-******************************************************************************/
-// init the PVision sensor
 void PVision::init () {
     IRsensorAddress = 0xB0;
-    IRslaveAddress = IRsensorAddress >> 1;   // This results in 0x21 as the address to pass to TWI
+    IRslaveAddress = IRsensorAddress >> 1;
 
     Wire.begin();
-    // IR sensor initialize
+
     Write_2bytes(0x30,0x01); delay(10);
     Write_2bytes(0x30,0x08); delay(10);
     Write_2bytes(0x06,0x90); delay(10);
@@ -47,12 +30,11 @@ void PVision::init () {
 }
 
 byte PVision::read() {
-    //IR sensor read
     Wire.beginTransmission(IRslaveAddress);
     Wire.write(0x36);
     Wire.endTransmission();
 
-    Wire.requestFrom(IRslaveAddress, 16);        // Request the 2 byte heading (MSB comes first)
+    Wire.requestFrom(IRslaveAddress, 16);
     for (i=0; i<16; i++) {
        data_buf[i]=0;
     }
@@ -64,16 +46,16 @@ byte PVision::read() {
         i++;
     }
 
-    blobcount = 0;
+    pointCount = 0;
 
-    Blob1.X = data_buf[1];
-    Blob1.Y = data_buf[2];
-    s   = data_buf[3];
-    Blob1.X += (s & 0x30) <<4;
-    Blob1.Y += (s & 0xC0) <<2;
-    Blob1.Size = (s & 0x0F);
+    Point.X = data_buf[1];
+    Point.Y = data_buf[2];
+    s = data_buf[3];
+    Point.X += (s & 0x30) <<4;
+    Point.Y += (s & 0xC0) <<2;
+    Point.Size = (s & 0x0F);
 
-    blobcount |= (Blob1.Size < 15)? BLOB1 : 0;
+    pointCount |= (Point.Size < 15)? POINT : 0;
 
-    return blobcount;
+    return pointCount;
 }
--- firmware/PVision.h
+++ firmware/PVision.h
@@ -1,18 +1,12 @@
-// PVision library for interaction with the Pixart sensor on a WiiMote
-// This work was derived from Kako's excellent Japanese website
-// http://www.kako.com/neta/2007-001/2007-001.html
-
-// Steve Hobley 2009 - www.stephenhobley.com
+// Library derived from Steve Hobley and Kako
 
 #ifndef PVision_h
 #define PVision_h
 
 #include "application.h"
 
-// bit flags for blob
-#define BLOB1 0x01
+#define POINT 0x01
 
-// Returned structure from a call to readSample()
 struct Blob {
    	int X;
    	int Y;
@@ -25,14 +19,12 @@ class PVision {
 public:
   PVision();
 
-	void init();   // returns true if the connection to the sensor established correctly
-	byte read();   // updated the blobs, and returns the number of blobs detected
+	void init();
+	byte read();
 
-	// Make these public
-	Blob Blob1;
+	Blob Point;
 
 private:
-  	// per object data
 	int IRsensorAddress;
 	int IRslaveAddress;
 	byte data_buf[16];
@@ -40,7 +32,7 @@ private:
 	int s;
 
 	void Write_2bytes(byte d1, byte d2);
-	byte blobcount; // returns the number of blobs found - reads the sensor
+	byte pointCount;
 
 };
 
